19110eb894ca6d6dd0b2fb4f2b452fcecab83782
Okal Otieno <okal@justokal.com>
2015-09-02 16:31:14 +0300

    Add build-freezing async test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 * Add test namespaces into ./test/test-runner.cljs
 * lein cljsbuild auto test
+
+Based on https://gitlab.com/keeds/cljsinit
--- test/cljsinit/core_test.cljs
+++ test/cljsinit/core_test.cljs
@@ -1,11 +1,23 @@
 (ns cljsinit.core-test
   (:require
-   [cljs.test :refer-macros [deftest testing is]]
+   [cljs.test :refer-macros [deftest testing is async]]
    [cljsinit.core :as c]))
 
+(deftest true-things-are-true
+  (testing "This is bound to work"
+    (is true)))
 
-(deftest base
-  (testing "base"
-    (is (= 1 1))
-    (is (= 1 "A"))
-    (is (= "ABCDE" (c/upper "abcde")))))
+(deftest false-things-are-not
+  (testing "This is bound to fail"
+    (is false)))
+
+(deftest async-things-are-what?
+  ;; Comment this out to see the previous two tests run
+  (testing "Let's wait and see"
+    (async done
+      (js/setTimeout
+        (fn []
+          (is true)
+          (done))
+        0))))
+
