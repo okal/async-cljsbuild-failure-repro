60dedd1abbfe2f6c823df36886517c4124a460a4
Okal Otieno <okal@justokal.com>
2015-09-02 16:38:20 +0300

    Pare down example

--- test/cljsinit/core_test.cljs
+++ test/cljsinit/core_test.cljs
@@ -3,21 +3,9 @@
    [cljs.test :refer-macros [deftest testing is async]]
    [cljsinit.core :as c]))
 
-(deftest true-things-are-true
-  (testing "This is bound to work"
-    (is true)))
-
-(deftest false-things-are-not
-  (testing "This is bound to fail"
-    (is false)))
-
-(deftest async-things-are-what?
-  ;; Comment this out to see the previous two tests run
-  (testing "Let's wait and see"
-    (async done
-      (js/setTimeout
-        (fn []
-          (is true)
-          (done))
-        0))))
-
+(deftest example-with-timeout
+  (async done
+         (js/setTimeout (fn []
+                          (is true)
+                          (done))
+                        0)))
